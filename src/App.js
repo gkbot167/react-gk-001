cacbe096128d5fdee44391397d905a1ff18edb00
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-20 07:31:29 +0000

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import FunctionComponent from './functionComponent';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
       <FunctionComponent></FunctionComponent>
     </div>
   );
--- src/functionComponent.js
+++ src/functionComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './expense.css';
 const FunctionComponent = () => {
   return (
-    <div>
-      <div> Date </div>
-      <div>
+    <div className="expense-item">
+      <div className="expense-item__description">
         <h2> Title </h2>
+      </div>
+      <div className="expense-item__price">
         <div> Amount </div>
       </div>
     </div>
