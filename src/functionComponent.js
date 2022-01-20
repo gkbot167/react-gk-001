98efb6bb99b9e78397cda21c78dbe530f278fd9c
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-20 07:46:27 +0000

    1 file modified

--- src/functionComponent.js
+++ src/functionComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './expense.css';
 const FunctionComponent = () => {
+  const ExpenseTitle = 'Car insurance';
   return (
     <div className="expense-item">
+      <h2>Date</h2>
       <div className="expense-item__description">
-        <h2> Title </h2>
+        <h2> {ExpenseTitle}</h2>
       </div>
       <div className="expense-item__price">
         <div> Amount </div>
