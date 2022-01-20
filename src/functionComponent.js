cea696ebdbb3119ca344977eabb55935e312b5b1
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-20 07:06:07 +0000

    2 files modified

--- src/functionComponent.js
+++ src/functionComponent.js
@@ -1,5 +1,13 @@
-const functionComponent = (name) => {
-  console.log(name);
+import React from 'react';
+const FunctionComponent = () => {
+  return (
+    <div>
+      <div> Date </div>
+      <div>
+        <h2> Title </h2>
+        <div> Amount </div>
+      </div>
+    </div>
+  );
 };
-functionComponent('GK');
-export default functionComponent;
+export default FunctionComponent;
