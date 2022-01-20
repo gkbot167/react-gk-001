ab9b7685731bf251ecc2de6a10842e03956b7258
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-20 10:19:30 +0000

    1 file modified, 1 added

--- src/ExpenseDate.js
+++ src/ExpenseDate.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './ExpenseDate.css';
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-UK', { month: 'long' });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString('en-UK', { day: '2-digit' });
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
