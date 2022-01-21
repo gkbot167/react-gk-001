336d149960c851fcdaab41c5fdc5ebdb2c601101
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-21 07:20:28 +0000

    1 file modified, 1 added

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
-import './Expense.css';
+import './Expenses.css';
 const Expenses = (props) => {
   return (
-    <div className="expense-item">
-      <div className="expense-item__description">
+    <div className="expenses">
+      <div className="">
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
         />
+        <br></br>
         <ExpenseItem
           title={props.items[1].title}
           amount={props.items[1].amount}
           date={props.items[1].date}
         />
+        <br></br>
         <ExpenseItem
           title={props.items[2].title}
           amount={props.items[2].amount}
