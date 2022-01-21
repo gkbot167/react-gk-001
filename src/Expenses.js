82f7ce53a920ae3a63de03a52ccdd5d679c0283d
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-21 06:20:32 +0000

    1 file added, 1 deleted

---
+++ src/Expenses.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+funtion Expenses(props);
+{
+  return (
+    <div>
+      <ExpenseItem
+        title={props.Expense[0].title}
+        amount={props.Expense[0].amount}
+        date={props.Expense[0].date}
+      />
+      <ExpenseItem
+        title={props.Expense[1].title}
+        amount={props.Expense[1].amount}
+        date={props.Expense[1].date}
+      />
+      <ExpenseItem
+        title={props.Expense[2].title}
+        amount={props.Expense[2].amount}
+        date={props.Expense[2].date}
+      />
+    </div>
+  );
+}
+export default Expenses;
