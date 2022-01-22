6dc92d1f5a595c1926294ff9ed2bf7e25427a1eb
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-22 07:59:19 +0000

    1 file modified, 2 added

---
+++ src/Card..js
@@ -0,0 +1,7 @@
+import React from 'react.js';
+import './Card.css';
+function Card(props) {
+  const classes = 'card' + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+export default Card;
--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Expense.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2> {props.title}</h2>
-      </div>
-      <div className="expense-item__price">
-        <div> {props.amount} </div>
+        <div className="expense-item__price">{props.amount} </div>
       </div>
     </div>
   );
