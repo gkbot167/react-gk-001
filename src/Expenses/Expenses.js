692e84fd11042b991608296e339086c137054dbc
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-22 09:05:52 +0000

    11 files added, 11 deleted

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from './Card';
 const Expenses = (props) => {
   return (
     <div className="expenses">
