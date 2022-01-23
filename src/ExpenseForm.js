b088aee1bfe027710102a835aebb04226fdf5dcc
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-23 06:19:43 +0000

    1 file modified

--- src/ExpenseForm.js
+++ src/ExpenseForm.js
@@ -4,21 +4,21 @@ const ExpenseForm = () => {
   return (
     <form>
       <div className="new-expense__controls">
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
           <input type="text"></input>
         </div>
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Amount</label>
           <input type="number" min="1" step="1"></input>
         </div>
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Date</label>
           <input type="date" min="2021-01-01" max="2022-12-31"></input>
         </div>
-        <div className="new-expense__action">
-          <button>Add Expense</button>
-        </div>
+        <br></br>
+        <div className="new-expense__action"></div>
+        <button>Add Expense</button>
       </div>
     </form>
   );
