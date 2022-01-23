c4bed5736f31de5838d6c88908ee15e8748160e9
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-23 14:52:12 +0000

    1 file modified

--- src/ExpenseForm.js
+++ src/ExpenseForm.js
@@ -17,11 +17,11 @@ const ExpenseForm = () => {
     //});
   };
   const AmountClickHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setEnteredAmount(event.target.value);
     //setUserInput({ ...userInput, enteredAmount: event.target.value });
   };
   const DateClickHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setEnteredDate(event.target.value);
     //setUserInput({ ...userInput, enteredDate: event.target.value });
   };
   const submitHandler = (event) => {
@@ -39,25 +39,31 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__controls ">
           <label>Title</label>
-          <input type="text" onChange={TitleClickHandler}></input>
+          <div>
+            <input type="text" onChange={TitleClickHandler}></input>
+          </div>
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
-          <input
-            type="text"
-            onChange={AmountClickHandler}
-            min="1"
-            step="1"
-          ></input>
+          <div>
+            <input
+              type="number"
+              onChange={AmountClickHandler}
+              min="1"
+              step="1"
+            ></input>
+          </div>
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input
-            type="text"
-            onChange={DateClickHandler}
-            min="2021-01-01"
-            max="2022-12-31"
-          ></input>
+          <div>
+            <input
+              type="date"
+              onChange={DateClickHandler}
+              min="2021-01-01"
+              max="2022-12-31"
+            ></input>
+          </div>
         </div>
         <br></br>
         <div className="new-expense__action"></div>
