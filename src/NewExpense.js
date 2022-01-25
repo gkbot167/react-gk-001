ebbd75c0f5d86177f69b592c1cc319987e25a394
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-25 13:11:29 +0000

    1 file modified

--- src/NewExpense.js
+++ src/NewExpense.js
@@ -1,20 +1,14 @@
 import React from 'react';
+
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
+
 const NewExpense = () => {
-  const saveExpenseDataHandler = (enteredexpensesData) => {
-    const expensesData = {
-      ...enteredexpensesData,
-      id: Math.random().toString(),
-    };
-    console.log(expensesData);
-    return (
-      <div className="new-expense">
-        <ExpenseForm
-          saveExpenseDataHandler={saveExpenseDataHandler}
-        ></ExpenseForm>
-      </div>
-    );
-  };
-  export default NewExpense;
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
 };
+
+export default NewExpense;
