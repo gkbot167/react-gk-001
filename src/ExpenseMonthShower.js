c9accafe3601a34ba9d37251a8d74bdd3607fa5c
gkbot167 <gokulkrish16071999@gmail.com>
2022-01-25 15:27:23 +0000

    1 file modified, 1 added

--- src/ExpenseMonthShower.js
+++ src/ExpenseMonthShower.js
@@ -1,46 +1,55 @@
 import React from 'react';
-
+import ExpenseMonthShowerTube from './ ExpenseMonthShowerTube';
 const ExpenseMonthShower = () => {
   return (
     <div>
       <div>
         <label>Jan</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Feb</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Mar</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Apr</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>May</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Jun</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Jul</label>
-      </div>
-      <div>
-        <label>JAN</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Aug</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Sep</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Oct</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Nov</label>
+        <ExpenseMonthShowerTube />
       </div>
       <div>
         <label>Dec</label>
+        <ExpenseMonthShowerTube />
       </div>
     </div>
   );
---
+++ src/ExpenseMonthShowerTube.js
@@ -0,0 +1,5 @@
+import React from './react';
+const ExpenseMonthShowerTube = () => {
+  return <div></div>;
+};
+export default ExpenseMonthShowerTube;
